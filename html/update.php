a15cd8ec88fe7b2a12c7ebd3e3a086c048c3bf62
sharppaul <weerheim.paul@gmail.com>
2017-02-07 21:54:30 +0100

    Started working on PHP update code

--- html/update.php
+++ html/update.php
@@ -1,6 +1,22 @@
 <?php
+$servername = "localhost";
+$username = "solr";
+$password = "kaas";
+
+if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
+    echo 'We don\'t have mysqli!!!';
+} else {
+    echo 'Phew we have it!';
+}
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 
 
 
-
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
 ?>
