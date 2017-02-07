cfcbeadec11eac1f1f07feacab0c5293af033f54
root <root@solr-server.localdomain>
2017-02-07 21:34:05 +0000

    Final commit to this fucking php shit

--- html/update.php
+++ html/update.php
@@ -2,17 +2,17 @@
 $servername = "localhost";
 $username = "solr";
 $password = "kaas";
+$database = "solr";
 
 if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
-	#http_response_code(400);
-	#exit();
-	echo "wtf1";
+	http_response_code(400);
+	exit();
 } 
  
 
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 
 
 
@@ -22,20 +22,21 @@ if ($conn->connect_error) {
 } 
 
 if ( !isset($_POST['time']) || !isset($_POST['value']) ){
-        #http_response_code(400);
-        #exit();
-	echo "wtf2";
-
+		http_response_code(400);
+		exit();
 }
-
-$sql = "INSERT INTO data (time,value) VALUES (".mysql_real_escape_string(($_POST['time'])).",".mysql_real_escape_string ($_POST['value']).");";
+$sql = "INSERT INTO data (time,value) VALUES (".
+    mysqli_real_escape_string($conn, $_POST['time']).
+    ", ".
+    mysqli_real_escape_string($conn, $_POST['time']).
+    ");";
 
 if ($conn->query($sql) === TRUE) {
-	echo "VALUES (".mysql_real_escape_string(($_POST['time'])).",".mysql_real_escape_string ($_POST['value']).")";    
+		echo "VALUES (".mysql_real_escape_string(($_POST['time'])).",".mysql_real_escape_string ($_POST['value']).")";
 } else {
-	#http_response_code(500);
-        #exit();
-	echo "wtf3";
+		http_response_code(400);
+   		exit();
 }
 
 ?>
+
