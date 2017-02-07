6e65a7a62c8ffb1c899139e162e8d8e7817be3ab
root <root@solr-server.localdomain>
2017-02-07 21:15:23 +0000

    Worked on server updating

--- html/update.php
+++ html/update.php
@@ -4,10 +4,12 @@ $username = "solr";
 $password = "kaas";
 
 if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
-    echo 'We don\'t have mysqli!!!';
-} else {
-    echo 'Phew we have it!';
-}
+	#http_response_code(400);
+	#exit();
+	echo "wtf1";
+} 
+ 
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
@@ -16,7 +18,24 @@ $conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    	die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
+
+if ( !isset($_POST['time']) || !isset($_POST['value']) ){
+        #http_response_code(400);
+        #exit();
+	echo "wtf2";
+
+}
+
+$sql = "INSERT INTO data (time,value) VALUES (".mysql_real_escape_string(($_POST['time'])).",".mysql_real_escape_string ($_POST['value']).");";
+
+if ($conn->query($sql) === TRUE) {
+	echo "VALUES (".mysql_real_escape_string(($_POST['time'])).",".mysql_real_escape_string ($_POST['value']).")";    
+} else {
+	#http_response_code(500);
+        #exit();
+	echo "wtf3";
+}
+
 ?>
